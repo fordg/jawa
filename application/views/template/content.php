de4483cec6a1cd5c5a1b316acbf0d255780dc342
Rico Oktavian Adhi Wibowo <rico@lussa.net>
2015-06-15 00:44:58 +0700

    update
    #update grid

--- application/views/template/content.php
+++ application/views/template/content.php
@@ -122,256 +122,47 @@
     <!-- end search box -->
 
     <!-- recent properties -->
-    <section id="recent-properties-slider" class="wrap recent-properties recent-properties-slider">
+    <section class="section-center">
         <div class="container">
-            <div class="recent-properties-inner">
-                <div class="section-title">
-                    <h3>Recent Properties</h3>
+            <div class="col-md-4">
+                <div class="main-title">
+                    Info Hunian
                 </div>
-                <div class="recent-properties-content">
-                    <div class="caroufredsel-wrap">
-                        <ul>
-                            <li>
-                                <div class="property-row">
-                                    <article class="hentry has-featured">
-                                        <div class="property-featured">
-                                            <a class="content-thumb" href="property-details.html">
-                                                <img src="<?php echo base_url('fe/images/property/property1.jpg') ?>" class="attachment-property-thumb" alt="" />
-                                            </a>
-                                            <span class="property-category">
-                                                <a href="type-single-family-home.html">Single Family Home</a>
-                                            </span>
-                                            <div class="property-detail">
-                                                <div class="size">
-                                                    <span>1913 sqft</span>
-                                                </div>
-                                                <div class="bathrooms">
-                                                    <span>2</span>
-                                                </div>
-                                                <div class="bedrooms">
-                                                    <span>4</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="property-wrap">
-                                            <h2 class="property-title"><a href="property-details.html" title="Visalia, NJ 93277">Visalia, NJ 93277</a></h2>
-                                            <div class="property-excerpt">
-                                                <p>This 4 bedroom home sits on an oversized lot at the end of a cul-de-sac...</p>
-                                            </div>
-                                        </div>
-                                        <div class="property-summary">
-                                            <div class="property-info">
-                                                <div class="property-price">
-                                                    <span><span class="amount">&#36;154,500</span></span>
-                                                </div>
-                                                <div class="property-action">
-                                                    <a href="property-details.html">More Details</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </article>
 
-                                    <article class="hentry has-featured">
-                                        <div class="property-featured">
-                                            <a class="content-thumb" href="property-details.html">
-                                                <img src="<?php echo base_url('fe/images/property/property1.jpg') ?>" class="attachment-property-thumb" alt="" />
-                                            </a>
-                                            <span class="property-category"><a href="type-single-family-home.html">Single Family Home</a></span>
-                                            <div class="property-detail">
-                                                <div class="size">
-                                                    <span>1118 sqft</span>
-                                                </div>
-                                                <div class="bathrooms">
-                                                    <span>2</span>
-                                                </div>
-                                                <div class="bedrooms">
-                                                    <span>3</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="property-wrap">
-                                            <h2 class="property-title"><a href="property-details.html" title="Single Family Residential, NJ">Single Family Residential, NJ</a></h2>
-                                            <div class="property-excerpt">
-                                                <p>Classic 60's ranch living. House has hardwood floors and hard coat plaster walls and ceilings...</p>
-                                            </div>
-                                        </div>
-                                        <div class="property-summary">
-                                            <div class="property-info">
-                                                <div class="property-price">
-                                                    <span><span class="amount">&#36;299,000</span></span>
-                                                </div>
-                                                <div class="property-action">
-                                                    <a href="property-details.html">More Details</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </article>
-                                </div>
-                                <div class="property-row">
-                                    <article class="hentry has-featured">
-                                        <div class="property-featured">
-                                            <a class="content-thumb" href="property-details.html">
-                                                <img src="<?php echo base_url('fe/images/property/property1.jpg') ?>" class="attachment-property-thumb" alt="" /> 
-                                            </a>
-                                            <span class="property-category"><a href="type-apartment.html">Apartment</a></span>
-                                            <div class="property-detail">
-                                                <div class="size">
-                                                    <span>1304 sqft</span>
-                                                </div>
-                                                <div class="bathrooms">
-                                                    <span>2</span>
-                                                </div>
-                                                <div class="bedrooms">
-                                                    <span>3</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="property-wrap">
-                                            <h2 class="property-title">
-                                                <a href="property-details.html" title="Peter Cooper Village">Peter Cooper Village</a>
-                                            </h2>
-                                            <div class="property-excerpt">
-                                                <p>
-                                                    Peter Cooper Village/ Stuyvesant Town provides an unbeatable combination of city energy and community tranquility,...
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div class="property-summary">
-                                            <div class="property-info">
-                                                <div class="property-price">
-                                                    <span><span class="amount">&#36;5,109</span></span>
-                                                </div>
-                                                <div class="property-action">
-                                                    <a href="property-details.html">More Details</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </article>
+                <div class="main-container">
+                    <ul class="nav nav-vertical nav-stacked">
+                        <li role="presentation"><a href="#">Home</a></li>
+                        <li role="presentation"><a href="#">Profile</a></li>
+                        <li role="presentation"><a href="#">Messages</a></li>
+                    </ul>
+                </div>
+            </div>
 
-                                    <article class="hentry has-featured">
-                                        <div class="property-featured">
-                                            <a class="content-thumb" href="property-details.html">
-                                                <img src="<?php echo base_url('fe/images/property/property1.jpg') ?>" class="attachment-property-thumb" alt="" /> 
-                                            </a>
-                                            <span class="property-category"><a href="type-condo.html">Condo</a></span>
-                                            <div class="property-detail">
-                                                <div class="size">
-                                                    <span>1,856 sqft</span>
-                                                </div>
-                                                <div class="bathrooms">
-                                                    <span>2</span>
-                                                </div>
-                                                <div class="bedrooms">
-                                                    <span>2</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="property-wrap">
-                                            <h2 class="property-title">
-                                                <a href="property-details.html" title="Ocala, FL34481">Ocala, FL34481</a>
-                                            </h2>
-                                            <div class="property-excerpt">
-                                                <p>
-                                                    Wonderful expanded end-unit Augusta featuring 2 bedrooms in split-bedroom plan, study/den/library off of cathedral-ceilinged HUGE...
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div class="property-summary">
-                                            <div class="property-info">
-                                                <div class="property-price">
-                                                    <span><span class="amount">&#36;95,000</span></span>
-                                                </div>
-                                                <div class="property-action">
-                                                    <a href="property-details.html">More Details</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </article>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="property-row">
-                                    <article class="hentry has-featured">
-                                        <div class="property-featured">
-                                            <a class="content-thumb" href="property-details.html">
-                                                <img src="<?php echo base_url('fe/images/property/property1.jpg') ?>" class="attachment-property-thumb" alt="" /> 
-                                            </a>
-                                            <span class="property-category"><a href="type-single-family-home.html">Single Family Home</a></span>
-                                            <div class="property-detail">
-                                                <div class="size">
-                                                    <span>123 sqft</span>
-                                                </div>
-                                                <div class="bathrooms">
-                                                    <span>1</span>
-                                                </div>
-                                                <div class="bedrooms">
-                                                    <span>1</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="property-wrap">
-                                            <h2 class="property-title">
-                                                <a href="property-details.html" title="New York Upper West Side">New York Upper West Side</a>
-                                            </h2>
-                                            <div class="property-excerpt">
-                                                <p>Fantastic One Bedroom Facing East In This Amazing Trump Place Doorman Building. There Is A...</p>
-                                            </div>
-                                        </div>
-                                        <div class="property-summary">
-                                            <div class="property-info">
-                                                <div class="property-price">
-                                                    <span><span class="amount">&#36;3,350</span></span>
-                                                </div>
-                                                <div class="property-action">
-                                                    <a href="property-details.html">More Details</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </article>
+            <div class="col-md-4">
+                <div class="main-title">
+                    Info Destinasi
+                </div>
 
-                                    <article class="hentry has-featured">
-                                        <div class="property-featured">
-                                            <a class="content-thumb" href="property-details.html">
-                                                <img src="<?php echo base_url('fe/images/property/property1.jpg') ?>" class="attachment-property-thumb" alt="" /> 
-                                            </a>
-                                            <span class="property-category"><a href="type-apartment.html">Apartment</a></span>
-                                            <div class="property-detail">
-                                                <div class="size">
-                                                    <span> 1752 sqft</span>
-                                                </div>
-                                                <div class="bathrooms">
-                                                    <span>2</span>
-                                                </div>
-                                                <div class="bedrooms">
-                                                    <span>2</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="property-wrap">
-                                            <h2 class="property-title">
-                                                <a href="property-details.html" title="AVA High Line">AVA High Line</a>
-                                            </h2>
-                                            <div class="property-excerpt">
-                                                <p>Special pricing for immediate move-ins! Call for details! AVA is a first. Our apartments are...</p>
-                                            </div>
-                                        </div>
-                                        <div class="property-summary">
-                                            <div class="property-info">
-                                                <div class="property-price">
-                                                    <span><span class="amount">&#36;3,410</span></span>
-                                                </div>
-                                                <div class="property-action">
-                                                    <a href="property-details.html">More Details</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </article>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <a class="caroufredsel-prev" href="#"></a>
-                    <a class="caroufredsel-next" href="#"></a>
+                <div class="main-container">
+                    <ul class="nav nav-vertical nav-stacked">
+                        <li role="presentation"><a href="#">Home</a></li>
+                        <li role="presentation"><a href="#">Profile</a></li>
+                        <li role="presentation"><a href="#">Messages</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="main-title">
+                    Info Belanja
+                </div>
+
+                <div class="main-container">
+                    <ul class="nav nav-vertical nav-stacked">
+                        <li role="presentation"><a href="#">Home</a></li>
+                        <li role="presentation"><a href="#">Profile</a></li>
+                        <li role="presentation"><a href="#">Messages</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
